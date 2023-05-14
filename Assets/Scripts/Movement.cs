22d15ab46602e3edafd7abddbdfd7c4d1c123501
Marta <s24332@pjwstk.edu.pl>
2023-05-15 01:13:48 +0200

    TutorialDone
    Zrobiony tutorial, zaimportowany font

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,44 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    private Animator anim;
+    public Animator anim;
 
     public float upForce = 100;
     public float speed = 1500;
-    public float runSpeed = 2500;
 
     public bool isGrounded = false;
-    public bool isJump = false;
-    public bool isWalk = false;
+
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+     
     }
+
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+       
+
+        float move = Input.GetAxis("Horizontal");
+        if (move == 0)
         {
-            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * runSpeed * Time.deltaTime, rb.velocity.y);
-            isWalk = true;
+            anim.SetBool("IsWalk", false);
+            
         }
-        else 
+        else
         {
-            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, rb.velocity.y);
-            isWalk = true;
+            if (move > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else if (move < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+
+            
+                
+                anim.SetBool("IsWalk", true);
+                rb.velocity = new Vector2(move * speed * Time.deltaTime, rb.velocity.y);
+            
+
+            anim.SetFloat("yVelocity", rb.velocity.y);
+
         }
+
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            anim.SetBool("IsJump", true);
             rb.AddForce(Vector2.up * upForce);
             isGrounded = false;
-            isJump = true;
         }
 
     }
@@ -46,5 +65,9 @@ public class Movement : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         isGrounded = true;
+
+        anim.SetBool("IsJump", !isGrounded);
+
     }
+
 }
