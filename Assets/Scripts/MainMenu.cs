d033d311fac139c37f150b68f5aa9d6bfbdb5639
Marta <s24332@pjwstk.edu.pl>
2023-05-31 15:08:38 +0200

    MenuZrobione
    Działące main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,15 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject OptionMenu;
+    public GameObject ChapterMenu;
+    void Start()
+    {
+        OptionMenu.gameObject.SetActive(false);
+        ChapterMenu.gameObject.SetActive(false);
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("Tutorial");
     }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
 
     public void QuitGame()
     {
